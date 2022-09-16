f5c105fdc728c1d497f5b1d8188d35844fdc54a2
sridevim55 <77396349+sridevim55@users.noreply.github.com>
2022-09-16 08:16:05 +0530

    Pushed project

--- client_side/src/Header/Header.js
+++ client_side/src/Header/Header.js
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import "./Header.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { NotificationAdd } from '@mui/icons-material';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 function Header() {
   return (
-    <div>Header Paithiyamdi </div>
+    <div>
+      <div className='header_main'>
+        <div className='header_left'>
+          <p><AccountBalanceIcon /></p>
+          <p>Billling</p>
+        </div>
+        <div className='header_center'>
+          
+         <p><input className='header_search' type={"search"} placeholder={"search bill"}></input></p>
+   
+        </div>
+        <div className='header_right'>
+          <p><NotificationAdd /></p>
+          <p><SettingsApplicationsIcon /></p>
+          <p><AccountCircleIcon /></p>
+        </div>
+      </div>
+
+
+
+
+
+
+    </div>
   )
 }
 
